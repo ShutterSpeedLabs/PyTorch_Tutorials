3c08b1ec674e070b043dc572c109c6594b865ddd
kisna-aryan <testseries0abc@gmail.com>
2024-01-30 21:02:19 +0530

    a

--- Pytorch_Tensors/tensor_math.py
+++ Pytorch_Tensors/tensor_math.py
@@ -2,3 +2,26 @@ import torch
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
+x = torch.tensor([1, 2, 3])
+y = torch.tensor([7, 8, 9])
+
+#Addition
+z1 = torch.empty(3)
+torch.add(x, y, out=z1)
+
+z2 = torch.add(x, y)
+
+z = x + y
+print(z)
+
+#Subtraction
+z_sub = x - y
+print(z_sub)
+
+#Division
+z_div = torch.true_divide(x, y)
+print(z_div)
+
+#inplace operations
+
+
