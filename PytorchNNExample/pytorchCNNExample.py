4644069ba9383ad0d593e9dc3680ad11350e54d7
ShutterSpeed Labs <shutterspeedlabs@gmail.com>
2024-06-21 22:10:43 +0530

    train

--- PytorchNNExample/pytorchCNNExample.py
+++ PytorchNNExample/pytorchCNNExample.py
@@ -19,6 +19,28 @@ class NN(nn.Module):
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
+class CNN(nn.Module):
+    def __init__(self, in_channels=1, num_classes=10):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(3, 3), stride=(1, 1), padding=(1,1))
+        self.pool = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3,3), stride=(1,1), padding=(1,1))
+        self.fc1 = nn.Linear(16*7*7, num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc1(x)
+        return x
+def save_checkpoint(state, filename="my_checkPoint.pth.tar"):
+    print("=> Saving checkpoint")
+    torch.save(state, filename)
+
+def load_checkpoint(checkpoint):
+    print("=> Loading checkpoint")
 
 #Set device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -29,7 +51,7 @@ num_classes = 10
 learning_rate = 0.001
 batch_size = 64
 num_epoches = 10
-
+load_model = False
 
 #Load data
 train_dataset = datasets.MNIST(root='dataset/', train=True, transform=transforms.ToTensor(), download=True)
@@ -40,19 +62,29 @@ test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=Tr
 
 
 #Initialize netowrk
-model = NN(input_size=input_size, num_classes=num_classes).to(device)
+#model = NN(input_size=input_size, num_classes=num_classes).to(device)
+model = CNN().to(device)
 
 #Loss and optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate) 
 
+if load_model:
+    load_checkpoint(torch.load("my_checkpoint.pth.tar"))
+
 #Train Network
 for epoch in range(num_epoches):
+    losses = []
+
+    if epoch%3==0:
+        checkpoint = {'state_dict':model.state_dict(), 'optimizer':optimizer.state_dict()}
+        save_checkpoint(checkpoint)
+
     for batch_idx, (data, targets) in enumerate(train_loader):
         data = data.to(device=device)
         targets = targets.to(device=device)
         # #print(data.shape)
-        data = data.reshape(data.shape[0], -1)
+        # data = data.reshape(data.shape[0], -1)
 
         #forward
         scores = model(data)
@@ -79,7 +111,7 @@ def check_accuracy(loader, model):
         for x, y in loader:
             x = x.to(device= device)
             y = y.to(device= device)
-            x = x.reshape(x.shape[0], -1)
+          #  x = x.reshape(x.shape[0], -1)
 
             scores = model(x)
             _, predictions = scores.max(1)
